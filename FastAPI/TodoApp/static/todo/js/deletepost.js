2a21e2ad8aad1dffd56717411e22b208bd92b8b0
Accelerate-with-DML <accelerate@digimantra.com>
2022-05-06 15:45:20 +0530

    fastapi-todo

---
+++ FastAPI/TodoApp/static/todo/js/deletepost.js
@@ -0,0 +1,9 @@
+let deletePost=confirm("");
+    if(deletePost){
+        console.log("Your post has been deleted successfully");
+    }
+    else{
+        console.log("Your post has not been deleted");
+    }
+
+    
